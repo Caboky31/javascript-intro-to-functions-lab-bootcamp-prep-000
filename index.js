f2942b6b396eff727bfa5227dffbd3c445753e0d
caboky31@hotmail.com Bernard <caboky31@hotmail.com>
2018-09-05 17:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === uppecase ) {
+  if (string.toLowerCase() === lowercase ) {
     return ("I can't hear you!")
-  } else if  (string.toLowerCase() === lowercase){
+  } else if  (string.toUpperCase() === uppercase){
     return ("YES INDEED!")
   } else if (string === "I love you, Grandma.") {
     return ("I love you, too")
